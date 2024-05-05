df05407734f7cda89e8c852fd73b57474e5db321
Andrew Bardukov <arhandreu@gmail.com>
2024-05-05 18:41:14 +0500

    Добавлен вывод списка мебели

--- homew/views.py
+++ homew/views.py
@@ -1,8 +1,11 @@
-import datetime
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Furniture
 
 
 def date_now(request):
-    return HttpResponse(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    list_furniture = Furniture.objects.all()
+    for item in list_furniture:
+        print(item.image)
+    return render(request, "homew/index.html", {"furniture": list_furniture, "title": "Список мебели"})
